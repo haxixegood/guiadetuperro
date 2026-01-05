6fa3c2afc08ce01fcdd124e7e2afc7cc7611150b
haxixegood <pedroquista@hotmail.com>
2026-01-05 17:20:21 -0300

    fix: update vite config for vercel deployment

--- SITE/vite.config.ts
+++ SITE/vite.config.ts
@@ -1,5 +1,10 @@
-import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
